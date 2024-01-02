b9572cd58b44369398fc5f72e6642252358e3aa2
unknown <suhailalsabith2@gmail.com>
2024-01-02 23:57:26 +0530

    added

---
+++ ecommerce_project/ecommerce_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name='ecommerce_app'
+
+urlpatterns = [
+
+    path('',views.allProdCat,name='allProdCat'),
+    path('<slug:c_slug>/',views.allProdCat,name='products_category'),
+    path('<slug:c_slug>/<slug:product_slug>/',views.proDetail,name='pro_category')
+]
